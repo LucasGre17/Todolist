fc7d0633cbaedde3d8b13ecabef60e0973e752b4
Lucas <lucas.grezil@etudiant.univ-lr.fr>
2022-03-18 16:30:31 +0100

    feat : show findall tache

--- app/models/Tache.java
+++ app/models/Tache.java
@@ -3,19 +3,22 @@ package models;
 import play.db.jpa.Model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
 public class Tache extends Model {
 
     @Id
+    @GeneratedValue
     public Long id;
     public String nom;
     public String description;
 
-    public Tache(Long id, String nom, String description) {
-        this.id = id;
+    public Tache(String nom, String description) {
         this.nom = nom;
         this.description = description;
     }
+
+
 }
