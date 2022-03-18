08520da96747aee9f77be9435b0d256137cca702
LucasG <lucasgrezil17@gmail.com>
2022-03-18 12:16:18 +0100

    feat : add model Tache

--- app/models/Tache.java
+++ app/models/Tache.java
@@ -2,6 +2,20 @@ package models;
 
 import play.db.jpa.Model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Tache extends Model {
-	
+
+    @Id
+    public Long id;
+    public String nom;
+    public String description;
+
+    public Tache(Long id, String nom, String description) {
+        this.id = id;
+        this.nom = nom;
+        this.description = description;
+    }
 }
